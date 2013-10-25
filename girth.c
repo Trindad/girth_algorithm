a287e1801cf6b6b24475f99a649c91f4413a1c27
Silvana Trindade <syletri@gmail>
2013-10-25 17:16:26 -0200

    Update girth.c

--- girth.c
+++ girth.c
@@ -14,20 +14,38 @@
 int grau[MAX];
 int visited[MAX];
 int fila[MAX];
-int T[MAX];
 
-int DFS(int i,int **A,int n);
-int LCA(int x,int y);
 int num_arestas(int **A, int n);
 int grau_maximo(int **A, int n);
 int grau_minimo(int **A, int n);
 int cintura(int **A, int n);
 
 
+
+#include <stdio.h>
+#include <stdlib.h> 
+
+#define MIN(a,b) (((a)<(b) && (a))?(a):(b))
+#define MAX 100000
+#define INF 9999 /*numero que será inserido no vetor de distancia*/
+int grau[MAX]; /*graus encontrados na matriz*/
+int visited[MAX];
+int fila[MAX];
+
+
+
+int num_arestas(int **A, int n);
+int grau_maximo(int **A, int n);
+int grau_minimo(int **A, int n);
+int cintura(int **A, int n);
+int BFS(int **A,int n,int i);
+
+
 int main(void)
 {
 
 	printf("\t\tMatriz Adjacência Simétrica\t\n\n");
+
 	int n = 0;
 	int i,j;
 
@@ -41,12 +59,6 @@ int main(void)
 	}
 	else
 	{
-
-
-		for(i = 0;i < n; i++){ 
-
-	        visited[i] = 0;
-		}
 
 		int **A = (int**) malloc (sizeof(int*)*(n));
 
@@ -71,6 +83,9 @@ int main(void)
 			}
 		}
 
+		/*
+			Preenchimento da matriz A
+		*/
 		for (i = 0; i < n-1; i++)
 		{
 			for (j = i+1; j < n; j++)
@@ -79,34 +94,17 @@ int main(void)
 				{
 					printf("%d chega em %d? (0/1) ", i,j);
 					scanf("%d",&A[i][j]);
-
-					A[j][i] = A[i][j];
+					A[j][i] = A[i][j]; /* faz espelhamento dos valores*/
 						
 				}
 			}
 		}
 		
 		/*
-			Copia da matriz para manipular na função que calcula numero de arestas
+			Chamadas das funções
 		*/
-		int **temp = (int**) malloc (sizeof(int*)*(n));
-
-		for (i = 0; i < n; i++)
-		{
-			temp[i] = (int*) malloc (sizeof(int)*(n));
-
-			if (temp[i] == NULL)
-			{
-				exit(1);
-			}
-
-			for (j = 0; j < n; j++)
-			{
-				temp[i][j] = A[i][j];
-				
-			}
-		}
-		printf("E(G) = %d \n",num_arestas(temp,n));
+		
+		printf("E(G) = %d \n",num_arestas(A,n));
 		printf("delta(G) = %d \n",grau_maximo(A,n));
 		printf("sigma(G) = %d \n",grau_minimo(A,n));
 		int girth = cintura(A,n);
@@ -120,12 +118,14 @@ int main(void)
 			printf("g(G) = infinito\n\n");
 		}
 		
-		
+		/* 
+			imprime matriz adjacência
+		*/
 		for (i = 0; i < n; i++)
 		{
 			for (j = 0; j < n; j++)
 			{
-				printf(" %d", A[i][j]);
+				printf("\t%d\t", A[i][j]);
 				
 			}
 			printf("\n");
@@ -139,20 +139,14 @@ int num_arestas(int **A, int n) {
 
 	int arestas = 0,i,j;
 
+	/*
+		percorre acima da diagonal principal para obter numero de arestas
+	*/
 	for (i = 0; i < n-1; i++)
 	{
 			for (j = i+1; j < n; j++)
 			{
-				if (A[i][j] == 1 && A[j][i] == 1)
-				{
-					arestas = arestas + 1;
-					A[j][i] = 0;
-					A[j][i] = 0;
-				}	
-				else if (A[i][j] == 1)
-				{
-					arestas++;
-				}
+				arestas += A[i][j];
 			}
 	}
 
@@ -179,10 +173,15 @@ int grau_maximo(int **A, int n) {
 			}	
 		}
 		grau[i] = grau_maximo;
+
+		/*
+			Verifica se o numero atual é maior que o anterior.
+		*/
+
 		if ( grau_maximo > max && (grau_maximo <= ((n*(n-1))/2)) && grau_maximo != 0 )
 		{
 			max = grau_maximo > max ? grau_maximo : max; /* teste se grau_maximo encontrado é maior que o valor atual de minimo*/
-			//printf("%d\n",max );
+			
 		}
 		grau_maximo = 0;
 		i++;
@@ -193,7 +192,7 @@ int grau_maximo(int **A, int n) {
 int grau_minimo(int **A, int n) {
 
 	int *grau,k = 0,j = 0;
-	int min = 9999;
+	int min = n+1;
 
 	grau = (int*) malloc (sizeof(int)*n);
 
@@ -215,108 +214,108 @@ int grau_minimo(int **A, int n) {
 			}	
 
 		}
+		/*
+			Verifica se atual é menor que o anterior.
+		*/
 		min = grau[k] < min && (grau[k] <= ((n*(n-1))/2)) && grau != 0 ? grau[k] : min; /* teste se grau encontrado é menor que o valor atual de minimo*/
-		k = k + 1;
+		k ++;
 	}
+
 	return min;
 }
-int DFS(int v,int **A,int n)
-{
-   /* int i = 0;
 
-    visited[v] = 1;
+/*
 
-    for( ;i < n; i++) {
+	Função percorre a matriz utilizando a função BSF() para obter a cintura , ou seja, o menor ciclo de um grafo,
+	seguindo os conceitos de grafos simples.
 
-        if(!visited[A[v][i]])
-        {
-        	printf("%d   %d\n",v,i );
-            DFS(i,A,n);
-        }
-    }
-    return v;*/
-}
-
-int LCA(int x,int y) /* ultimo ancestral comun*/
-{/*
-        while(visited[x] != visited[y]) {
-
-        	if(L[x] > L[y])
-        	{
-        		x = visited[x];
-        	}
-            else
-            {
-            	 y = visited[y];
-            }
-        }
-                
-        while( x != y)
-        {
-        	if(L[x] > L[y])
-        	{
-        		 x = T[x];
-        	}
-            else
-            {
-            	 y = T[y];
-            }
-                       
-        } */return 0;            
-}
+*/
 
 int cintura(int **A, int n) {
 
+	/*
+		condição garante mais chances de ter cintura o grafo
+	*/
 	if (n < 3 || A == NULL)
 	{
 		printf("ERROR OU INFINITO\n");
 		return 0;
 	}
 
-	int girth = 9999,orig,i,distancia[n];
-	int ini, fim;
+	
+	int origem,girth = n+1;
 
-	for (orig = 0; orig < n; orig++)
+	for (origem = 0; origem < n; origem++)
 	{
 
-		for( i = 0; i < n; i++)
+		int cintura_origem = BSF(A,n,origem); /* percorre por largura */
+
+		/*
+			Verifica se a cintura da origem é menor que a anterior, caso for substitui
+		
+		*/	
+		if (girth > cintura_origem)
 		{
-		  	distancia[i] = INF;
-		}
-
-		distancia[orig] = 0;
-
-		ini = fim = 0;
-
-		fila[fim++] = orig;
-
-		while(ini != fim) 
-		{
-			int no = fila[ini++];
-			
-			printf("grau:%d\n",grau[no]);
-
-			for( i = 0; i < grau[no]; i++) 
-			{
-			    int viz = A[no][i];
-			    
-			    if(distancia[viz] == INF ) 
-			    {
-			    	
-					fila[fim++] = viz;
-					distancia[viz] = distancia[no] + 1;
-			    }
-			    else
-			    {
-			    	int temp = MIN(girth,distancia[no] + distancia[viz] );
-			    	girth = temp >= 3 ? temp : girth;
-			    	printf("%d\n",girth );
-			    }
-			}
+			girth = cintura_origem;
 		}
 		
 	}
-	printf("%d\n",girth );
 	return girth;
 }
 
+/*
+	Função que obtêm a cintura apartir de uma origem. 
+	
+*/
+
+int BSF(int **A,int n, int origem) {
+
+	int i,girth = n+1,*distancia;
+	int ini, fim;
+
+	distancia = (int*) malloc (sizeof(int)*n);
+
+	if (distancia == NULL)
+	{
+		printf("Erro na alocação de memória\n");
+		exit(1);
+	}
+
+	/*
+		Preenche vetor de distancia com um numero 'infinito'
+	*/
+	for( i = 0; i < n; i++)
+	{
+		  	distancia[i] = INF;
+	}
+
+	distancia[origem] = 0;
+
+	ini = fim = 0;
+
+	fila[fim++] = origem; 
+
+	while(ini != fim) 
+	{
+		int no = fila[ini++];
+
+		for( i = 0; i < grau[no]; i++) 
+		{
+		    int viz = A[no][i];
+		    
+		    if(distancia[viz] == INF ) 
+		    {
+		    	
+				fila[fim++] = viz;
+				distancia[viz] = distancia[no] + 1;
+		    }
+		    else
+		    {
+		    	int temp = MIN(girth,distancia[no] + distancia[viz] +1);
+		    	girth = temp >= 3 ? temp : girth;
+		    	
+		    }
+		}
+	}
+	return girth;
+}
